861439f1a84f2ff0339413753df89f097a4b2ea3
Taylor <tahlor@gmail.com>
2020-07-21 11:13:12 -0600

    update

--- scripts/prep_for_aug.sh
+++ scripts/prep_for_aug.sh
@@ -7,10 +7,8 @@ cp -r $original_path $original_path_NOA
 
 # Change to no adaptation
 cd $original_path_NOA
-replace $original_path $original_path_NOA .sh
-Y
-replace $original_path $original_path_NOA .yaml
-Y
+find . -type f -name "resume.sh"   -exec sed -i 's/$original_path/$original_path_NOA/' {} \;
+find . -type f -name "RESUME.yaml" -exec sed -i 's/$original_path/$original_path_NOA/' {} \;
 sed -i 's/    //g' resume.sh
 # sed ':a;N;$!ba;s|\n#!/bin/bash|#!/bin/bash|g' resume.sh # test
 sed -i ':a;N;$!ba;s|\n#!/bin/bash|#!/bin/bash|g' resume.sh
@@ -26,12 +24,21 @@ sed -i ':a;N;$!ba;s|\n#!/bin/bash|#!/bin/bash|g' resume.sh
 
 # LR
 # find . -type f -name "*$3" -exec sed -i "s@$1@$2@g" {} \;
-sed -i 's/.*reset_LR.*/reset_LR: true/' RESUME.yaml
-sed -i 's/.*learning_rate.*/learning_rate: 1e-05/' RESUME.yaml
-sed -i 's/.*scheduler_gamma:.*/scheduler_gamma: 0.97/' RESUME.yaml
+# Recursive replace
+find . -type f -name "RESUME.yaml" -exec sed -i 's/.*reset_LR.*/reset_LR: true/' {} \;
+find . -type f -name "RESUME.yaml" -exec sed -i 's/.*learning_rate.*/learning_rate: 1e-05/' {} \;
+find . -type f -name "RESUME.yaml" -exec sed -i 's/.*scheduler_gamma:.*/scheduler_gamma: 0.97/'  {} \;
+find . -type f -name "RESUME.yaml" -exec sed -i 's|dtw_no_truncation/2020|dtw_no_truncation/no_adaptation/2020|'  {} \;
 
-sed -i 's|dtw_no_truncation/2020|dtw_no_truncation/with_adaptation/2020|' RESUME.yaml
-sed -i 's|dtw_no_truncation/2020|dtw_no_truncation/with_adaptation/2020|' resume.sh
+find . -type f -name "resume.sh" -exec sed -i ':a;N;$!ba;s|\n#!/bin/bash|#!/bin/bash|g' {} \;
+find . -type f -name "resume.sh" -exec sed -i 's|dtw_no_truncation/2020|dtw_no_truncation/no_adaptation/2020|' {} \;
+
+#sed -i 's/.*reset_LR.*/reset_LR: true/' RESUME.yaml
+#sed -i 's/.*learning_rate.*/learning_rate: 1e-05/' RESUME.yaml
+#sed -i 's/.*scheduler_gamma:.*/scheduler_gamma: 0.97/' RESUME.yaml
+#
+#sed -i 's|dtw_no_truncation/2020|dtw_no_truncation/with_adaptation/2020|' RESUME.yaml
+#sed -i 's|dtw_no_truncation/2020|dtw_no_truncation/with_adaptation/2020|' resume.sh
 
 
 cp all_stats.json all_stats_backup.json
