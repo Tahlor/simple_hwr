5ad4507b9ba3356cb1504a1ca12062a0072ad1e6
Taylor <tahlor@gmail.com>
2020-08-10 22:50:41 -0600

    update

---
+++ paper/stats_dtw.py
@@ -0,0 +1,11 @@
+# Online
+    # On test set
+    # DTW - resample (and no resample)
+        # L1/L2
+
+    # NN - resample and no resample
+        # table of predictions
+        # table of GTs
+
+# Offline
+    # NN - table of predictions to nearest point -
