90eddfc56ae12b8e1d465ce366ac63c9dc017fda
Taylor <tahlor@gmail.com>
2021-07-26 18:11:01 -0600

    update message

--- paper/rename.py
+++
@@ -1,7 +0,0 @@
-from pathlib import Path
-import os
-root = Path("/media/data/GitHub/simple_hwr/RESULTS/ONLINE_PREDS/RESUME_model/new_experiment33/imgs")
-
-for i in root.rglob("*"):
-    new = i.parent / i.name[2:]
-    os.rename(i,new)
--- synthesis/synth_utils/constants.py
+++
@@ -1,3 +0,0 @@
-class Global:
-    train_mean = 0.0
-    train_std = 1.0
