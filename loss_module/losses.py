9c2c6099802cec18c6e5a1a4260720e48af3bc77
Taylor <tahlor@gmail.com>
2021-09-15 16:56:13 -0600

    update message

--- loss_module/losses.py
+++ loss_module/losses.py
@@ -643,6 +643,64 @@ class L1(CustomLoss):
 
         return loss  # , to_value(loss)
 
+class NLL(CustomLoss):
+    """
+
+    """
+
+    def __init__(self, loss_indices, **kwargs):
+        """
+        """
+        super().__init__(loss_indices, **kwargs)
+        self.lossfun = self.nll
+        self.dim = 20
+
+    def nll(self, preds, targs, label_lengths, mask, **kwargs):
+        return NLL.compute_nll_loss(targs, preds, mask, M=self.dim)
+
+    @staticmethod
+    def compute_nll_loss(targets, y_hat, mask, M=20):
+        epsilon = 1e-6
+        split_sizes = [1] + [20] * 6
+        y = torch.split(y_hat, split_sizes, dim=2)
+
+        eos_logit = y[0].squeeze()
+        log_mixture_weights = F.log_softmax(y[1], dim=2)
+
+        mu_1 = y[2]
+        mu_2 = y[3]
+
+        logstd_1 = y[4]
+        logstd_2 = y[5]
+
+        rho = torch.tanh(y[6])
+
+        log_constant = log_mixture_weights - math.log(2 * math.pi) - logstd_1 - \
+            logstd_2 - 0.5 * torch.log(epsilon + 1 - rho.pow(2))
+
+        x1 = targets[:, :, 1:2] # targets: Batch, Width, GT_size 32,1191,3
+        x2 = targets[:, :, 2:]
+
+        std_1 = torch.exp(logstd_1) + epsilon
+        std_2 = torch.exp(logstd_2) + epsilon
+
+        X1 = ((x1 - mu_1) / std_1).pow(2) # X1: BATCH, 1191 (# of stroke points), 1; mu_1: 32,1191,20 (20 gaussians)
+        X2 = ((x2 - mu_2) / std_2).pow(2)
+        X1_X2 = 2 * rho * (x1 - mu_1) * (x2 - mu_2) / (std_1 * std_2)
+
+        Z = X1 + X2 - X1_X2
+
+        X = -Z / (2 * (epsilon + 1 - rho.pow(2)))
+
+        log_sum_exp = torch.logsumexp(log_constant + X, 2)
+        BCE = nn.BCEWithLogitsLoss(reduction='none')
+
+        loss_t = -log_sum_exp + BCE(eos_logit, targets[:, :, 0])
+        loss = torch.sum(loss_t * mask)
+
+        return loss
+
+
 class L2(CustomLoss):
     """ Use opts to specify "variable_L1" (resample to get the same number of GTs/preds)
     """
